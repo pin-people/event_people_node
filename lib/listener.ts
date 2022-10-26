7e738a6a1e26ea63d9eeaba07e52fd082b3033ff
alan alves <alan.demit@hotmail.com>
2022-10-26 17:45:21 -0300

    docs(lib): adding method docs

--- lib/listener.ts
+++ lib/listener.ts
@@ -4,16 +4,26 @@ import { Event } from './event';
 import { MissingAttributeError } from './utils/errors';
 
 export class Listener {
+	/**
+	 * Calls the broker consume method to receive stream events from certain queue
+	 * @param {string} eventName string for queue event name
+	 * @param callback action callback function to execute after consuming the event
+	 */
 	public static on(
 		eventName: string,
 		callback: (event: Event, context: Context) => void,
-	) {
+	): void {
 		if (eventName.length <= 0) throw new MissingAttributeError('Event name');
 
 		Config.broker.consume(eventName, callback);
 	}
 
-	private consumedEventName(eventName: string) {
+	/**
+	 *Normilizes event name with 'all' suffix
+	 * @param {string} eventName string name for the event
+	 * @returns {String} string
+	 */
+	private consumedEventName(eventName: string): string {
 		return eventName.split('.').length <= 3 ? `${eventName}.all` : eventName;
 	}
 }
